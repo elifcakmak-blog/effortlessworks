69ad5647dfc7eceff5f76421f95ad970a0b4c763
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-11 19:09:13 -0500

    fix error

--- app/7/certifications/page.tsx
+++ app/7/certifications/page.tsx
@@ -3,11 +3,9 @@
 import CustomCursor from '../../0/0-0-cursor/page';
 import Navigation from '../../0/1-5-0-info-nav/navigation';
 import Footer from '../../0/0-2-footer/footer'
-import { usePathname } from 'next/navigation'; // Import to get the current path
 import UnderConstruction from '../../0/0-4-UnderConstruction/UnderConstruction'; // Import the new component
 
 export default function Home() {
-  const pathname = usePathname(); // Get current path
 
   return (
     <div className="home-container">
